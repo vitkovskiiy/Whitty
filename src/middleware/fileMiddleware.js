2b5f452801ce938eeeae2d9cb71805053e0ee13d
Ivan Vitkovskiy <cloudfly97@icloud.com>
2026-03-22 15:00:39 +0200

    Now middleware for upload files are has limit for file size < 4mb

--- src/middleware/fileMiddleware.js
+++ src/middleware/fileMiddleware.js
@@ -2,13 +2,19 @@
 const multer = require("multer");
 const path = require("path");
 
+const maxSize = 4000000;
+
 const storage = multer.diskStorage({
-  destination(req, file, cb) {    
+  destination(req, file, cb) {
+    if (req.headers['content-length']> maxSize) {
+      cb(new Error("File should be less than 4mb"))
+    }
     cb(null, path.join(__dirname, "../../uploads/avatars"));
   },
   filename(req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
+     cb(null, Date.now() + "-" + file.originalname);
   },
+  onFileUploadStart: function (file, req, res) {},
 });
 
 const types = ["image/png", "image/jpeg", "image/jpg"];
