95b1a85a3f5c668018fbdd7efc2527af24d9e93b
Ivan Vitkovskiy <cloudfly97@icloud.com>
2026-03-24 11:48:52 +0200

    LIKE USE CASE: refactored on layered arch

--- src/controllers/upload.avatar.controller.js
+++ src/controllers/upload.avatar.controller.js
@@ -1,22 +1,24 @@
-const  UploadAvatarService = require("../services/avatar.upload.service")
+const UploadAvatarService = require("../services/avatar.upload.service");
 
- class UploadController {
-        async uploadAvatar(req,res) {
-            try {
-                const userId = req.user.id;
-                const avatarUrl = `/uploads/avatars/${req.file.filename}`;
-                
-                const uploadNewAvatar = await UploadAvatarService.uploadNewAvatar(userId,avatarUrl);
-                
-                res.status(201).json({
-                    message: "Avatar updated", 
-                    avatar:uploadNewAvatar,
-                })
-            } catch(error) {
-                 res.status(400).json({error: error.message})
-            }
-        }
- }
+class UploadController {
+  async uploadAvatar(req, res) {
+    try {
+      const userId = req.user.id;
+      const avatarUrl = `/uploads/avatars/${req.file.filename}`;
 
+      const uploadNewAvatar = await UploadAvatarService.uploadNewAvatar(
+        userId,
+        avatarUrl,
+      );
+
+      res.status(201).json({
+        message: "Avatar updated",
+        avatar: uploadNewAvatar,
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+}
 
 module.exports = new UploadController();
--- src/repositories/post.repository.js
+++ src/repositories/post.repository.js
@@ -41,5 +41,69 @@ class PostRepository {
     });
     return allPosts;
   }
+
+  async createComment(post_id, user_id, comment_text) {
+    const createComment = await prisma.comment.create({
+      data: {
+        post_id: Number(post_id),
+        user_id: user_id,
+        comment_text: comment_text,
+        created_at: new Date(),
+      },
+      include: {
+        user: {
+          select: {
+            username: true,
+            avatar: true,
+          },
+        },
+      },
+    });
+    return createComment;
+  }
+  async existingLike(user_id, post_id) {
+    const existingLike = await prisma.like.findUnique({
+      where: {
+        user_id_post_id: {
+          user_id: user_id,
+          post_id: Number(post_id),
+        },
+      },
+    });
+    return existingLike;
+  }
+  async createLike(user_id, post_id) {
+    const createLike = await prisma.$transaction(async (tx) => {
+      const addLike = await tx.like.create({
+        data: {
+          user_id: user_id,
+          post_id: Number(post_id),
+        },
+      });
+
+      const updateLike = await tx.post.update({
+        where: { post_id: Number(post_id) },
+        data: { likes_count: { increment: 1 } },
+      });
+    });
+    return createLike;
+  }
+  async deleteLike(user_id, post_id) {
+    const deleteLike = await prisma.$transaction(async (tx) => {
+      const removeLike = await tx.like.delete({
+        where: {
+          user_id_post_id: {
+            user_id: user_id,
+            post_id: Number(post_id),
+          },
+        },
+      });
+      const updatePost = await tx.post.update({
+        where: { post_id: Number(post_id) },
+        data: { likes_count: { decrement: 1 } },
+      });
+      return deleteLike;
+    });
+  }
 }
 module.exports = new PostRepository();
