ddff86fab002aa149036bbaa4555caeefd417547
Ivan Vitkovskiy <cloudfly97@icloud.com>
2026-03-24 11:47:57 +0200

    made more readable and clean post controller handle req datas

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -5,10 +5,14 @@ class PostController {
     console.log(req.file);
     const avatarUrl = req.file.path;
     const userId = req.user.id;
-    const caption = req.body.caption;
-    const content = req.body.content;
+    const { content, caption } = req.body;
 
-    const createPost = PostService.createPost(userId, avatarUrl, caption, content);
+    const createPost = PostService.createPost(
+      userId,
+      avatarUrl,
+      caption,
+      content,
+    );
     if (createPost) {
       res.status(200).json({
         message: "Post created successfully",
@@ -20,9 +24,36 @@ class PostController {
     }
   }
 
-  async getAllPost(req,res) {
-    const getAllPost = await PostService.getAllPosts()
-    return getAllPost;
+  async getAllPost(req, res) {
+    const getAllPost = await PostService.getAllPosts();
+    if(!getAllPost){
+      res.status(500).json({
+        message: "Error fetch all posts",
+      })
+    }
+    res.send(getAllPost)
+  }
+  async createComment(req, res) {
+    const { post_id, comment_text } = req.body;
+    const user_id = req.user.id;
+    const createComment = await PostService.createComment(post_id,user_id,comment_text);
+    if (!createComment) {
+      res.status(500).json({
+        message: "Error when tried to create comment ;(",
+      });
+    }
+    res.status(200).send(createComment);
+  }
+  async likePost(req, res) {
+    const { post_id } = req.body;
+    const user_id = req.user.id;
+
+    const likePost = await PostService.likePost(user_id, post_id);
+    if (!likePost) {
+      res.status(500).json({
+        message: "Error while liking",
+      });
+    }
   }
 }
 
