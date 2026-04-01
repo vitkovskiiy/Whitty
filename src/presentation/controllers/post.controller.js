a4dd6f1bb5adbe4e74a0bbf2057f8b91a4c1a39e
Ivan Vitkovskiy <cloudfly97@icloud.com>
2026-04-01 22:03:34 +0300

    fixed a async bug (forget to add a async and code are crashed)

--- src/presentation/controllers/post.controller.js
+++ src/presentation/controllers/post.controller.js
@@ -2,57 +2,81 @@ const PostService = require("../../application/services/post.service");
 
 class PostController {
   async createPost(req, res) {
-    console.log(req.file);
     const avatarUrl = req.file.filename;
     const userId = req.user.id;
     const { content, caption } = req.body;
 
-    const createPost = PostService.createPost(
+    const createPost = await PostService.createPost(
       userId,
       avatarUrl,
       caption,
       content,
     );
     if (createPost) {
-      res.status(200).json({
+      return res.status(201).json({
         message: "Post created successfully",
       });
     } else {
-      res.status(401).json({
+      return res.status(401).json({
         message: "Failed",
       });
     }
   }
 
   async getAllPost(req, res) {
-    const getAllPost = await PostService.getAllPosts();
-    if(!getAllPost){
-      res.status(500).json({
-        message: "Error fetch all posts",
-      })
+    try {
+      const getAllPost = await PostService.getAllPosts();
+      res.status.json(getAllPost);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        return res.status(404).json({ message: error.message });
+      }
+      if (error instanceof DomainError) {
+        return res.status(400).json({ message: error.message });
+      }
+      console.log(error.message);
+      return res.status(500).json({ message: "Error on server side" });
     }
-    res.send(getAllPost)
   }
-  async createComment(req, res) {
-    const { post_id, comment_text } = req.body;
-    const user_id = req.user.id;
-    const createComment = await PostService.createComment(post_id,user_id,comment_text);
-    if (!createComment) {
-      res.status(500).json({
-        message: "Error when tried to create comment ;(",
-      });
-    }
-    res.status(200).send(createComment);
-  }
-  async likePost(req, res) {
-    const { post_id } = req.body;
-    const user_id = req.user.id;
 
-    const likePost = await PostService.likePost(user_id, post_id);
-    if (!likePost) {
-      res.status(500).json({
-        message: "Error while liking",
-      });
+  async createComment(req, res) {
+    try {
+      const { post_id, comment_text } = req.body;
+      const user_id = req.user.id;
+      const createComment = await PostService.createComment(
+        post_id,
+        user_id,
+        comment_text,
+      );
+      res.status(200).send(createComment);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        return res.status(404).json({ message: error.message });
+      }
+      if (error instanceof DomainError) {
+        return res.status(400).json({ message: error.message });
+      }
+      console.log(error.message);
+       return res.status(500).json({ message: "Error on server side" });
+    }
+  }
+
+  async likePost(req, res) {
+    try {
+      const { post_id } = req.body;
+      const user_id = req.user.id;
+
+      const likePost = await PostService.likePost(user_id, post_id);
+      res.status(200).json(likePost);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        return res.status(404).json({ message: error.message });
+      }
+      if (error instanceof DomainError) {
+        return res.status(400).json({ message: error.message });
+      }
+      console.log(error.message);
+       return res.status(500).json({ message: "Error on server side" });
     }
   }
 }
