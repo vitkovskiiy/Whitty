2469ede7d1051d100eeec13281943a7c5240de76
Ivan Vitkovskiy <cloudfly97@icloud.com>
2026-04-05 11:58:53 +0300

    add custom error sender to service

--- src/application/services/auth.service.js
+++ src/application/services/auth.service.js
@@ -2,7 +2,7 @@ const AuthRepository = require("../../infrastructure/repositories/auth.repositor
 const bcrypt = require("bcrypt");
 const jwt = require("../../infrastructure/utils/jwt.generate");
 const UserMapper = require("../../infrastructure/mappers/UserMapper")
-const {DomainError,NotFoundError} = require("../../domain/error");
+const {DomainError} = require("../../domain/error");
 
 class AuthService {
   async login(username, password) {
@@ -11,30 +11,29 @@ class AuthService {
     if (!user) {
       throw new Error("Can't find user");
     }
-
     const isMatchPassword = await bcrypt.compare(password, user.password);
 
     if (!isMatchPassword) {
-      throw new InvalidPasswordError("Password invalid,please try again");
+      throw new DomainError("Password invalid,please try again");
     }
     const token = await jwt.generateAccessToken(user.user_id);
     if (!token) {
-      throw new TokenCreateError("Error while generate token");
+      throw new DomainError("Error while generate token");
     }
     const mappedUser = UserMapper.toDomain(user);
     return { mappedUser, token };
   }
 
-  async register(usernameValueObject, passwordValueObject) {
+  async register(username, password) {
     
-    const findByUsername = await AuthRepository.findByUsername(username);
+    const findByUsername = await AuthRepository.findByUsername(username,password);
     if (findByUsername) {
-      throw new UserAlreadyExistsError("User already exists");
+      throw new DomainError("User already exists");
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const createUser = await AuthRepository.register(username, hashedPassword);
     if (!createUser) {
-      throw new Error("Something happened when tried to create user");
+      throw new DomainError("Something happened when tried to create user");
     }
     return UserMapper.toDomain(createUser);
   }
--- src/application/services/country.service.js
+++ src/application/services/country.service.js
@@ -1,10 +1,11 @@
+const { DomainError } = require("../../domain/error");
 const CountryRepository = require("../../infrastructure/repositories/country.repository");
 
 class CountryService {
   async loadCountries() {
     const loadCountries = await CountryRepository.loadCountries();
     if (!loadCountries) {
-      throw new Error("Fetching countries are failed");
+      throw new DomainError("Fetching countries are failed");
     }
     return loadCountries;
   }
@@ -12,7 +13,7 @@ class CountryService {
   async setCountry(user_id, country_id) {
     const setCountry = await CountryRepository.setCountry(user_id, country_id);
     if (!setCountry) {
-      throw new Error("Setting country are failed");
+      throw new DomainError("Setting country are failed");
     }
     return setCountry;
   }
