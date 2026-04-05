9891c5fd529ab1d1ecd2abf78dd5f70c6cf3b52d
Ivan Vitkovskiy <cloudfly97@icloud.com>
2026-04-05 11:57:28 +0300

    now service has DI principe

--- src/application/services/all.users.service.js
+++ src/application/services/all.users.service.js
@@ -1,18 +1,24 @@
-const UsersRepository = require("../../infrastructure/repositories/all.users.repository");
+const User = require("../../domain/entities/User");
+const { DomainError } = require("../../domain/error");
+const UserRepository = require("../../infrastructure/repositories/all.users.repository")
 
 class UsersService {
+  constructor(){
+    this.repository = new UserRepository();
+  }
   async fetchAll(myId) {
-    const findUsers = await UsersRepository.fetchAll(myId);
+    const findUsers = await this.repository.fetchAll(myId);
     if (!findUsers) {
-      throw new Error("Error get all users");
+      throw new DomainError("Error get all users");
     }
     return findUsers;
   }
   async findMe(user_id) {
-    const findMe = await UsersRepository.findMe(user_id);
+    const findMe = await this.repository.findMe(user_id);
     if (!findMe) {
-      throw new Error("User not found");
+      throw new DomainError("User not found");
     }
+    console.log(findMe)
     return findMe;
   }
 }
--- src/infrastructure/repositories/all.users.repository.js
+++ src/infrastructure/repositories/all.users.repository.js
@@ -25,4 +25,4 @@ class UsersRepository {
   }
 }
 
-module.exports = new UsersRepository();
+module.exports = UsersRepository;
